15bcd293009d85edfba6b405944a3518dda89d5c
Gary Tou <gtcowboybob@gmail.com>
2017-10-05 22:22:27 -0700

    changed text.java to test pull on eclipse. TEST #2, test#1 was sucessful

--- Edhesive/src/test.java
+++ Edhesive/src/test.java
@@ -7,7 +7,7 @@ public class test {
 		Scanner scan = new Scanner (System.in);
 		double val = scan.nextDouble();
 		System.out.println(val - 11.5);
-		System.out.println("testddddddttddddddd");
+		System.out.println("testddddddttdddfgh");
 		
 	}
 
