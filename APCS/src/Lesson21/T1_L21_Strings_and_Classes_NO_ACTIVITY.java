6b7fcdc23719fd10ced3b1c5444e40bb974a7e2f
Gary Tou (Home) <gtcowboybob@gmail.com>
2017-10-14 21:03:39 -0700

    corrected Lesson21 and Lesson22's no activity statement

---
+++ APCS/src/Lesson21/T1_L21_Strings_and_Classes_NO_ACTIVITY.java
@@ -0,0 +1,10 @@
+package Lesson21;
+
+public class T1_L21_Strings_and_Classes_NO_ACTIVITY {
+
+	public static void main(String[] args) {
+		System.out.println("T1_L21_Strings_and_Classes\nNO ACTIVITY");
+
+	}
+
+}
