f8fd3d096a7b3e051c45cd3bba3f55299311c5a2
Gary Tou (Home) <gtcowboybob@gmail.com>
2017-10-15 00:15:30 -0700

    added Assignment 4, not done

--- APCS/src/java_test.java
+++ APCS/src/java_test.java
@@ -2,13 +2,18 @@
 public class java_test {
 
 	public static void main(String[] args) {
-		String a = "hello";
+		String a = "QUY";
 		String b = "hi";
+		boolean swear = false;
 		
-		int test = a.compareTo(b);
 		
-		System.out.println(test);
+		if ((a.compareTo("QUY") == 0)) {
+			swear = true;
+			
+
+
+		}
+		System.out.println((swear));
 
 	}
-
 }
