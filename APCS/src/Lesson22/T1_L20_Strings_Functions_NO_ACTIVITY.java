6084975a7f66daa50e9d76fa9ba68dadd70dab2d
Gary Tou (Home) <gtcowboybob@gmail.com>
2017-10-14 21:02:18 -0700

    Added Lesson21 and Lesson22

---
+++ APCS/src/Lesson22/T1_L20_Strings_Functions_NO_ACTIVITY.java
@@ -0,0 +1,10 @@
+package Lesson22;
+
+public class T1_L20_Strings_Functions_NO_ACTIVITY {
+
+	public static void main(String[] args) {
+		System.out.println("T1_L20_Strings_Functions_NO_ACTIVITY\nNO ACTIVITY");
+
+	}
+
+}
