2245b65d6eafd6931f4c64560ca17b24465e7aa2
gar.tou000 <gar.tou000@HHS205D-G032.403.edu>
2017-10-24 14:02:44 -0700

    Changed graphics

--- APCS/src/_Home_Projects/Graphics.java
+++ APCS/src/_Home_Projects/Graphics.java
@@ -6,28 +6,26 @@ import java.awt.event.*;
 
 //http://zetcode.com/tutorials/javaswingtutorial/firstprograms/
 
-public class Graphics extends JFrame implements ActionListener{  
-    JTextField tf; JLabel l; JButton b;  
-    Graphics(){  
-        tf=new JTextField();  
-        tf.setBounds(50,50, 150,20);  
-        l=new JLabel();  
-        l.setBounds(50,100, 250,20);      
-        b=new JButton("Find IP");  
-        b.setBounds(50,150,95,30);  
-        b.addActionListener(this);    
-        add(b);add(tf);add(l);    
-        setSize(400,400);  
-        setLayout(null);  
-        setVisible(true);  
-    }  
-    public void actionPerformed(ActionEvent e) {  
-        try{  
-        String host=tf.getText();  
-        String ip=java.net.InetAddress.getByName(host).getHostAddress();  
-        l.setText("IP of "+host+" is: "+ip);  
-        }catch(Exception ex){System.out.println(ex);}  
-    }  
-    public static void main(String[] args) {  
-        new Graphics();  
-    } }  
+public class Graphics extends JFrame {
+
+    public void Graphics() {
+
+        initUI();
+    }
+
+    private void initUI() {
+        
+        setTitle("This is a window!!!!!!");
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+        	Graphics ex = new Graphics();
+            ex.setVisible(true);
+        });
+    }
+}
