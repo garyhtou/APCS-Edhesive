22e2e362161777d5030603be003ed624b9133354
gt <gt@DESKTOP-BBUKMVL>
2017-11-26 19:06:29 -0800

    updated arrayOdd to be scanable

--- src/_Class_Work/arrayOdd.java
+++ src/_Class_Work/arrayOdd.java
@@ -9,12 +9,45 @@ public class arrayOdd {
 		int [] Array2 = {2, 3, 4, 5, 6, 7, 8, 9};
 		int [] Array3 = {2, 10, 20, 21, 23, 24, 40, 55, 60, 61};
 		
-		sort(Array1, numOddEven(Array1));
-		System.out.println();
-		sort(Array2, numOddEven(Array2));
-		System.out.println();
-		sort(Array3, numOddEven(Array3));
-
+		boolean selected = false;
+		while(!selected) {
+			Scanner scan = new Scanner (System.in);
+			System.out.println("Would you like to scan an array or use one of the presets?");
+			String answer = scan.next();
+			int arrayNum = 0;
+			if(answer.equalsIgnoreCase("scan")) {
+				System.out.println("How many integers would you like to scan?");
+				arrayNum = scan.nextInt();
+				int[] scannedArray = new int [arrayNum];
+				for(int i = 0; i < scannedArray.length; i++) {
+					System.out.println("Please enter an integer for index " + i);
+					String input = scan.next();
+					if(input.toUpperCase().equals(input.toLowerCase())) {
+						int intInput = Integer.parseInt(input);
+						scannedArray[i] = intInput;
+					}
+					else {
+						System.out.println("that was not an integer");
+						i--;
+					}
+				}
+				System.out.println("");
+				sort(scannedArray, numOddEven(scannedArray));
+				selected = true;
+			}
+			else if(answer.equalsIgnoreCase("preset")){
+				sort(Array1, numOddEven(Array1));
+				System.out.println();
+				sort(Array2, numOddEven(Array2));
+				System.out.println();
+				sort(Array3, numOddEven(Array3));
+				selected = true;
+			}
+			else {
+				System.out.println("please enter either either \"scan\" or \"preset\"");
+				selected = false;
+			}
+		}
 	}
 	public static int[] numOddEven(int[] array) {
 		int odd = 0;
