998b9d76011c8280f3919f931177dfb785c2a0e7
gar.tou000 <gar.tou000@HHS205D-G018.403.edu>
2017-12-13 14:06:07 -0800

    tested

--- _src/test/java_test.java
+++ _src/test/java_test.java
@@ -1,30 +1,19 @@
 package test;
+import java.util.ArrayList;
+
 import _Interfaces.IGeneral;
 
 public class java_test implements IGeneral{
 
 	public static void main(String[] args) {
-		
-		forEach();
 
 	}
 	
 	public static void test() {
+		ArrayList<String> list = new ArrayList<String>();
+		int INDEX = 1;
+		String VALUE_REMOVED = "hi";
 		
-		
-		
-		
-		
-		
-		
+		list.add(INDEX, VALUE_REMOVED);		
 	}
-	
-	public static void forEach() {
-		String[] myArray = {"Asdf", "s2egsf", "asdf32sdf"};
-		
-		for (String hey : myArray) {
-			System.out.println(hey);
-		}	
-	}
-
 }
