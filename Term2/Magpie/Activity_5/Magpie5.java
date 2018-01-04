8d38e37e5af96f5cc4a044937c72d0184bba0b67
gar.tou000 <gar.tou000@HHS205D-G018.403.edu>
2018-01-04 13:59:59 -0800

    finished Magpie

--- Term2/Magpie/Activity_5/Magpie5.java
+++ Term2/Magpie/Activity_5/Magpie5.java
@@ -265,7 +265,9 @@ public class Magpie5
 	private String [] randomResponses = {"Interesting, tell me more",
 			"Hmmm.",
 			"Do you really think so?",
-			"You don't say."
+			"You don't say.",
+			"Really?",
+			"Oh"
 	};
 	
 }
