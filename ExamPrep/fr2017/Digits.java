ce1744ca0b4675a9c957da1a2debbcf0a6c35603
Gary Tou <gtcowboybob@gmail.com>
2018-01-25 23:16:01 -0800

    apcs test prep

---
+++ ExamPrep/fr2017/Digits.java
@@ -0,0 +1,28 @@
+package fr2017;
+
+import java.util.ArrayList;
+
+public class Digits {
+	private ArrayList<Integer> digitList;
+	
+	public static void main(String[] args) {
+		Digits d1 = new Digits(15704);
+		
+	}
+	
+	public Digits(int num) {
+		int length = Integer.toString(num).length();
+		for(int i = 0; i < length; i++) {
+			digitList.add(num%10);
+			num/=10;
+		}
+		System.out.print(num);
+	}
+	
+	public boolean isStriclyIncreasing() {
+		return false;
+		
+	}
+	
+	
+}
