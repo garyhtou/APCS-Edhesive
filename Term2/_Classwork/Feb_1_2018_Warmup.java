7e19711c1536cd414b181f827320b037a407df4f
gar.tou000 <gar.tou000@HHS205D-G018.403.edu>
2018-02-01 13:22:09 -0800

    finished warmup

---
+++ Term2/_Classwork/Feb_1_2018_Warmup.java
@@ -0,0 +1,33 @@
+package _Classwork;
+
+public class Feb_1_2018_Warmup {
+	public static void main(String[] args) {
+		
+	}
+	// Question 1:
+	public int numCount(int[] array, int num) {
+		int counter = 0;
+		for(int item : array) {
+			if(item == num) {
+				counter++;
+			}
+		}
+		return counter;
+	}
+
+	/* Question 2:
+		Output: 52
+	 */
+	
+	/* Question 1:
+		a. always true
+	 */
+	
+	/* Question 2:
+		d. r = d * 5.0 + 0.5
+	 */
+	
+	/* Question 3:
+		e. I, II, III
+	 */
+}
