9571b9089a0ba25b174e1c75b8e8445d33bc0d58
rpanthee9417 <31248369+rpanthee9417@users.noreply.github.com>
2018-09-06 14:58:47 -0500

    It now generates 40 too.
    L9A3 didn't generate 40 with the code that was provided. I fixed it so it will generate 40.

--- Term1/Lesson9/Lesson_9_Activity_Three.java
+++ Term1/Lesson9/Lesson_9_Activity_Three.java
@@ -13,7 +13,7 @@ import java.lang.Math;
 class Lesson_9_Activity_Three {
     public static void main(String[] args) {
       
-    	System.out.println((int)(Math.random() * 20)+40);
+    	System.out.println((int)(Math.random() * 21)+40);
 
 
     }
